211504454ed2244b4a4cfdaefe2c18d7f54e1502
ClemClemant <clem.apps@oxiafr.org>
2026-05-15 01:36:45 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,29 @@
 self.addEventListener('install', (e) => self.skipWaiting());
 self.addEventListener('activate', (e) => e.waitUntil(clients.claim()));
 
+// Gestion de la notification en arrière-plan
 self.addEventListener('message', (event) => {
     if (event.data.type === 'START_TIMER') {
         const delay = event.data.delay;
         const label = event.data.label;
 
-        // Le téléphone garde ce chrono en mémoire même écran éteint
         setTimeout(() => {
             self.registration.showNotification('OneVote', {
-                body: `Le chrono de ${label} est terminé ! C'est l'heure.`,
+                body: `Le délai pour le vote ${label} est terminé !`,
                 icon: 'icon.png',
-                vibrate: [500, 200, 500],
-                requireInteraction: true,
-                tag: 'vote-' + label // Permet d'avoir plusieurs notifs si plusieurs chronos finissent
+                vibrate: [500, 110, 500, 110, 450, 110, 200, 110],
+                tag: 'vote-' + label,
+                renotify: true,
+                requireInteraction: true
             });
         }, delay);
     }
+});
+
+// Redirection vers le site quand on clique sur la notification
+self.addEventListener('notificationclick', (event) => {
+    event.notification.close();
+    event.waitUntil(
+        clients.openWindow('https://oneblockfrance.fr/vote')
+    );
 });
