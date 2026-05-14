9ca7f21e39427685e9acb2b9c98a8afeedc117fb
ClemClemant <clem.apps@oxiafr.org>
2026-05-15 00:40:13 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,11 +6,9 @@ self.addEventListener('activate', (event) => {
     event.waitUntil(clients.claim());
 });
 
-// Écoute les messages de l'application pour lancer un chrono
 self.addEventListener('message', (event) => {
     if (event.data.type === 'START_TIMER') {
-        const delay = event.data.delay; // en millisecondes
-        
+        const delay = event.data.delay;
         setTimeout(() => {
             self.registration.showNotification('OneVote', {
                 body: 'Le compte à rebours est fini ! C\'est l\'heure de voter.',
@@ -19,13 +17,12 @@ self.addEventListener('message', (event) => {
                 vibrate: [200, 100, 200, 100, 200],
                 tag: 'vote-notification',
                 renotify: true,
-                requireInteraction: true // La notif reste jusqu'à ce que tu cliques
+                requireInteraction: true
             });
         }, delay);
     }
 });
 
-// Quand on clique sur la notification, ça ouvre l'app
 self.addEventListener('notificationclick', (event) => {
     event.notification.close();
     event.waitUntil(
